7f37a509181f71be0c377e9cb64f702e7b93ee2e
Karol Vidlár <karolko.vidlar@gmail.com>
2026-04-13 12:40:22 +0200

    me

--- searching.py
+++ searching.py
@@ -13,11 +13,22 @@ def read_data(file_name, field):
     cwd_path = Path.cwd()
     
     file_path = cwd_path / file_name
+def linear_search(searched_seq, num):
+    empty = {}
+    indexes = []
+    count = 0
+    for i,j in enumerate(searched_seq):
+        if j == num:
+            indexes.append(i)
+            count += 1
+    empty["Indexy:"] = indexes
+    empty["Pocet:"] = count
+    return empty
 
 
 def main():
 
-    return read_data("sequential.json", "unordered_numbers")
+    return linear_search(read_data("sequential.json", "unordered_numbers"), 0)
 
 sequential_data = main()
 print(sequential_data)
